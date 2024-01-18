cd06244335e15c69787a78ea3aa1968efa51f3c8
sabrinacaniato <126755749+sabrinacaniato@users.noreply.github.com>
2024-01-18 16:48:35 +0100

    Update prove.py

--- prove.py
+++ prove.py
@@ -3,7 +3,10 @@ import base64
 def hex2string(text):
     ascii_string =  bytes.fromhex(text).decode('utf-8', errors="ignore") 
     return ascii_string
-
+    
+# decode the flag from hex to ascii
+decoded = binascii.unhexlify(hex_encrypted)
+decoded = decoded.decode('ascii')
 
 def binary2string(text):
     return''.join(chr(int(text[i*8:i*8+8],2)) for i in range(len(text)//8))
